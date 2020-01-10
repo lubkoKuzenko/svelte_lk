fe9725aa4e180e40606ad3370e090f1b85b83f8e
Lyubomyr Kuzenko <l.kuzenko@levi9.com>
2020-01-10 13:01:21 +0200

    add logic blocks

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,11 @@
 
 	export let name = 'world';
 	export let age = 0;
+	let cats = [
+		{ id: 'J---aiyznGQ', name: 'Keyboard Cat' },
+		{ id: 'z_AbfPXTKms', name: 'Maru' },
+		{ id: 'OUtn3pvWmpg', name: 'Henri The Existential Cat' }
+	];
 
 	$: nameC = `<b>calculation</b> ${age}`;
 	$: if (age > 5){
@@ -18,10 +23,29 @@
 <main class="container">
 	<Title title={name} />
 	<h3>Age: {age}</h3>
+
+	{#if age < 3}
+		<p>nameC less than 3</p>
+	{:else}
+		<p>nameC more than 3</p>
+	{/if}
+
 	<p>{@html nameC}</p>
 	<input type="text" value={name} on:input={onInputChange} />
 	<input type="text" bind:value={name} />
 	<button on:click={onAgeChange}>Change Age</button>
+
+	<hr />
+	<h3>List</h3>
+	<ul>
+		{#each cats as { id, name }, i (id)}
+			<li>
+				<a target="_blank" href="https://www.youtube.com/watch?v={id}">{i + 1}: {name}</a>
+			</li>
+		{/each}
+	</ul>
+
+	<hr />
 </main>
 
 <style></style>
